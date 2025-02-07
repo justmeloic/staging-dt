88dc64eea577ce0e762ca89d2839c84feae5a080
Amit Verma <amiacs@gmail.com>
2025-02-07 17:22:21 +0100

    Correct linting errors

--- ran-guardian/event_scout/firestore_helper.py
+++ ran-guardian/event_scout/firestore_helper.py
@@ -1,10 +1,12 @@
+"""Helper functions to interfact with Firestore database."""
+
+import os
+from datetime import datetime, timezone, timedelta
 from google.cloud import firestore
 from google.cloud.firestore_v1.base_query import FieldFilter
 from google.cloud import bigquery
 import pandas
 import pandas_gbq
-import datetime
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,43 +15,69 @@ FIREBASE_DB_NAME = os.getenv("FIREBASE_DB_NAME")
 
 db = firestore.Client(project=PROJECT_ID, database=FIREBASE_DB_NAME)
 
-def get_all_event_types():
+def get_all_event_types() -> list[dict]:
+    """Returns a list of all event types."""
+
     docs = db.collection('event_types').stream()
     event_types = [doc.to_dict() for doc in docs]
+
     return event_types
 
 def get_locations(priority: str, days_since_last_scan: int) -> list[str]:
+    """Returns all locations with the specified priority and last scan date."""
 
-    num_days_ago = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=days_since_last_scan)
+    num_days_ago = datetime.now(timezone.utc) - timedelta(days=days_since_last_scan)
 
     if priority != "all":
-        docs = db.collection('locations').where(filter=FieldFilter("priority", "==", priority)).where(filter=FieldFilter("last_scanned", "<", num_days_ago)).stream()
+        docs = db.collection('locations').where(
+            filter=FieldFilter("priority", "==", priority)
+            ).where(
+                filter=FieldFilter("last_scanned", "<", num_days_ago)
+            ).stream()
     else:
-        docs = db.collection('locations').where(filter=FieldFilter("last_scanned", "<", num_days_ago)).stream()
+        docs = db.collection('locations').where(
+            filter=FieldFilter("last_scanned", "<", num_days_ago)
+            ).stream()
 
     locations = [doc.id for doc in docs if doc.id != "0_stats"]
     return sorted(locations)
 
-def update_last_scanned(location):
-    doc_ref = db.collection("locations").document(location)
-    doc_ref.set({u"last_scanned": datetime.datetime.now(tz=datetime.timezone.utc)}, merge=True)
+def update_last_scanned(location: str) -> None:
+    """Updates the last_scanned field of the given location."""
 
-def get_global_stats():
+    doc_ref = db.collection("locations").document(location)
+    doc_ref.set({"last_scanned": datetime.datetime.now(tz=datetime.timezone.utc)}, merge=True)
+
+def get_global_stats() -> dict:
+    """Returns the global scanning stats."""
+
     return db.collection("locations").document("0_stats").get().to_dict()
 
-def get_num_scanned_locations(num_days = 90):
-    num_days_ago = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=num_days)
-    docs = db.collection("locations").where(filter=FieldFilter("last_scanned", ">=", num_days_ago)).stream()
+def get_num_scanned_locations(num_days: int = 90) -> int:
+    """Returns the number of locations scanned in the last num_days days."""
+
+    num_days_ago = datetime.now(datetime.timezone.utc) - timedelta(days=num_days)
+    docs = db.collection("locations").where(
+        filter=FieldFilter("last_scanned", ">=", num_days_ago)
+        ).stream()
     locations = [doc.id for doc in docs]
+
     return len(locations)
 
-def get_unscanned_locations(num_days = 360):
-    num_days_ago = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=num_days)
-    docs = db.collection("locations").where(filter=FieldFilter("last_scanned", "<", num_days_ago)).stream()
+def get_unscanned_locations(num_days: int = 360) -> list[str]:
+    """Returns the list of locations that have not been scanned in the last num_days days."""
+
+    num_days_ago = datetime.now(timezone.utc) - timedelta(days=num_days)
+    docs = db.collection("locations").where(
+        filter=FieldFilter("last_scanned", "<", num_days_ago)
+        ).stream()
     locations = [doc.id for doc in docs]
+
     return sorted(locations)
 
-def save_events(location, events):
+def save_events(location: str, events: list[dict]) -> None:
+    """Writes events to Firestore and BigQuery."""
+
     for event in events:
         # Write event to the location collection
         _, doc_ref = db.collection(location).add(event)
@@ -70,11 +98,15 @@ def save_events(location, events):
 
     # Write event to BQ
     df = pandas.DataFrame(events)
-    pandas_gbq.to_gbq(df, "events_db_de.people_events", project_id=PROJECT_ID, if_exists='append', location='europe-west3')
+    pandas_gbq.to_gbq(df, "events_db_de.people_events", 
+                      project_id=PROJECT_ID, if_exists='append', 
+                      location='europe-west3')
 
     update_last_scanned(location)
 
-def get_events_by_location(location):
+def get_events_by_location(location: str) -> list[dict]:
+    """""Returns all events for the specified location."""
+
     events = db.collection(location).stream()
     events_list = [{**event.to_dict(), 'id': event.id} for event in events]
     return events_list
@@ -111,7 +143,9 @@ QUERY = """
     ) <= {radius}; -- 5000 meters = 5 kilometers
     """
 
-def get_nodes_within_radius(lng, lat, radius = 4000):
+def get_nodes_within_radius(lng: float, lat: float, radius: int = 4000) -> pandas.DataFrame:
+    """Get nodes within the specidied radius of the given coordinates."""
+
     client = bigquery.Client(project=PROJECT_ID, location='europe-west3')
     query_job = client.query(QUERY.format(lng=lng, lat=lat, radius=radius))  # API request
     df = query_job.to_dataframe()
